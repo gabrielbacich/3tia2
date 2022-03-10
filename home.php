e8e8314af9a738d5d6e1fc62bd3b6823e18f1687
Gabriel Bacich <gabrielbacich@hotmail.com>
2022-03-10 10:56:09 -0300

    a

--- home.php
+++ home.php
@@ -1,5 +1,6 @@
 <?php
-    include'connect.php';
+    include 'connect.php';
+    include 'checkLogin.php';
     $s="select*from reg where id='$_SESSION[id]'";
     $qu= mysqli_query($con, $s);
     $f=mysqli_fetch_assoc($qu);
