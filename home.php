0a427dc0f410f08881dd58a7b15a3c689e2b9076
Gabriel Bacich <gabrielbacich@hotmail.com>
2022-05-05 12:02:21 -0300

    produto

--- home.php
+++ home.php
@@ -1,6 +1,5 @@
 <?php
     include 'connect.php';
-    include 'checkLogin.php';
     $s="select*from reg where id='$_SESSION[id]'";
     $qu= mysqli_query($con, $s);
     $f=mysqli_fetch_assoc($qu);
@@ -25,7 +24,6 @@ echo $f['username'];?>
 <?php
 echo $f['password']."<br>";?>
     </td></tr>
-    <tr><td> Nome do produto</td>
 </table>
 <a href="edit.php">Editar</a>
 <a href="delete.php">Deletar</a>
